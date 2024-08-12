b43f9950a5617024e07dbf72d5d52e3d6058b70a
damian dev <inamida16@gmail.com>
2024-08-12 18:59:30 +0300

    app structure reset and first screen build finished

---
+++ app/login/index.jsx
@@ -0,0 +1,69 @@
+import { View, Text, Image, Pressable } from 'react-native';
+import React from 'react';
+import Colors from './../../constants/Colors';
+
+export default function LoginScreen() {
+  return (
+    <View
+      style={{
+        backgroundColor: 'white',
+        height: '100%',
+      }}
+    >
+      <Image
+        style={{
+          width: '100%',
+          height: 500,
+        }}
+        source={require('./../../assets/images/login.png')}
+      />
+      <View
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: 'poppinsBold',
+            fontSize: 30,
+            textAlign: 'center',
+          }}
+        >
+          Ready to make a new Friend?
+        </Text>
+        <Text
+          style={{
+            fontFamily: 'poppinsMedium',
+            fontSize: 18,
+            textAlign: 'center',
+            color: Colors.GRAY,
+          }}
+        >
+          Let's adopt a pet which you like and make their life happy
+        </Text>
+        <Pressable
+          style={{
+            padding: 14,
+            marginTop: 50,
+            backgroundColor: Colors.PRIMARY,
+            width: '100%',
+            borderRadius: 14,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: 'center',
+              color: 'white',
+              fontFamily: 'poppins-medium',
+              fontSize: 20,
+            }}
+          >
+            Get Started
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
